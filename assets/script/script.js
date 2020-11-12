1cfc580c52a4839e6b150b549b8239e10f4b35f3
Dj Meechlouch <Meechlouch@gmail.com>
2020-11-11 21:22:48 -0600

    Added functionality to display search history and search history is interactive

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,39 +5,60 @@ let weatherWord;
 let city;
 
 $(document).ready(function () {
+  let history = JSON.parse(window.localStorage.getItem("history")) || [];
+  //iterate through local storage history
+  for (let i = 0; i < history.length; i++) {
+    //passing the history data of local storage to a function named display History
+    displayHist(history[i]);
+  }
+
+  $("#historyList").on("click", "li", function () {
+    city = $(this).text();
+    apiCalls(city);
+  });
   // listen for city input
   $("#searchBtn").on("click", function () {
-    $("#weather-music-list").empty();
+    //$("#weather-music-list").empty();
     city = $("#citySearch").val();
-    // get weather word for current weather in that city
-    let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${OPENWEATHER_API_KEY}`;
-    $.ajax({
-      url: weatherApiUrl,
-      method: "GET",
-    }).then(function (response) {
-      weatherWord = response.weather[0].main;
-      displayWeather(response);
-      console.log(`weatherWord = ${weatherWord}, city = ${city}`);
-      let queryString = `?part=snippet&maxResults=25&q=${weatherWord}%20Music&type=playlist&key=${YOUTUBE_API_KEY}`;
-      let youtubeAPIURL = YOUTUBE_SEARCH_ENDPOINT + queryString;
-      $.ajax({
-        url: youtubeAPIURL,
-        method: "GET",
-      }).then(function (response) {
-        console.log(response);
-        // get a random playlist in the response
-        let randomInt = Math.floor(Math.random() * response.items.length);
-        let randomPlaylistId = response.items[randomInt].id.playlistId;
-        playlistId = randomPlaylistId;
-        createIframe(playlistId);
-      });
-    });
+    if (city) {
+      apiCalls(city);
+      if (history.indexOf(city) === -1) {
+        history.push(city);
+        window.localStorage.setItem("history", JSON.stringify(history));
+        displayHist(city);
+      }
+    }
   });
 });
 
+function apiCalls(city) {
+  let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${OPENWEATHER_API_KEY}`;
+  $.ajax({
+    url: weatherApiUrl,
+    method: "GET",
+  }).then(function (response) {
+    weatherWord = response.weather[0].main;
+    displayWeather(response);
+    console.log(`weatherWord = ${weatherWord}, city = ${city}`);
+    let queryString = `?part=snippet&maxResults=25&q=${weatherWord}%20Music&type=playlist&key=${YOUTUBE_API_KEY}`;
+    let youtubeAPIURL = YOUTUBE_SEARCH_ENDPOINT + queryString;
+    $.ajax({
+      url: youtubeAPIURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      // get a random playlist in the response
+      let randomInt = Math.floor(Math.random() * response.items.length);
+      let randomPlaylistId = response.items[randomInt].id.playlistId;
+      playlistId = randomPlaylistId;
+      createIframe(playlistId);
+    });
+  });
+}
+
 function displayWeather(response) {
-  let cityName = $(".nameTemp").html(response.name + " " + response.main.temp + " °F");
-  let weatherDay = $(".weatherDay").html(`It's a ${weatherWord} day`);
+  $(".nameTemp").html(response.name + " " + response.main.temp + " °F");
+  $(".weatherDay").html(`It's a ${weatherWord} day`);
   let weatherIcon = response.weather[0].icon;
   let iconUrl = "https://openweathermap.org/img/w/" + weatherIcon + ".png";
   weatherWord = response.weather[0].main;
@@ -60,4 +81,11 @@ function createIframe(playlistId) {
   iframe.attr("frameborder", "0");
   iframe.attr("allowfullscreen");
   $("#player").empty().append(iframe);
+}
+
+function displayHist(city) {
+  let liEl = $("<li>");
+  liEl.addClass("list-group-item");
+  liEl.append(city);
+  $("#historyList").prepend(liEl);
 }
